40afb7ecb3a87982b929fb233e4d23d9fde1acf8
cxxxr <g23tlm@gmail.com>
2019-03-01 17:43:34 +0900

    add

--- 02-Lem/mode.md
+++ 02-Lem/mode.md
@@ -1,6 +1,7 @@
 ## はじめに
 lemでの拡張機能の書き方についてです。
-ここではhogeを作りながらlemでの拡張の書き方を見ていきます。
+ここではredditの投稿一覧のビューアを作りながらlemでの拡張の書いていきます。
+プロジェクト名はposts-listとします。
 
 ## 
 lemの拡張機能はasdfのシステムとして扱います。
@@ -9,14 +10,85 @@ lemの拡張機能はasdfのシステムとして扱います。
 
 テンプレートを以下のコマンドで生成できます。
 
-$ ros init lem hoge
+$ ros init lem posts-list
 
-これでhogeのベースが出来ました。
-ディレクトリツリーは以下のようになるはずです。
+これでプロジェクトのベースが出来ました。
+ディレクトリツリーは以下のようになります。
 
 ```
-$ tree hoge
-hoge
-├── lem-hoge.asd
+$ tree posts-list
+posts-list
+├── lem-posts-list.asd
 └── main.lisp
 ```
+
+lem上で`M-x start-lisp-repl`とするとreplが表示されます。
+最初に作成したプロジェクトを読み込んでみます。
+
+```
+CL-USER> (ql:quickload :lem-posts-list)
+```
+
+エラーが出て読み込めない場合はパスが通っていない可能性が高いです。
+デフォルトでは$HOME/common-lispにパスが通ってあるので
+posts-listディレクトリを$HOME/common-lisp/以下に配置するのが簡単です。
+
+##
+redditの記事をsubredditを指定してjsonで取得してみます。
+一つの記事をpostという構造体にし、postのリストを返す処理を用意します。
+これ自体はlemとは関係ないのでposts.lispに分離します。
+
+```
+(defpackage #:lem-posts-list/posts
+  (:import-from #:jonathan)
+  (:import-from #:dexador)
+  (:import-from #:trivia
+                #:match
+                #:plist)
+  (:import-from #:quri)
+  (:use #:cl)
+  (:export #:post-title
+           #:post-url
+           #:post-author
+           #:fetch-posts))
+(in-package #:lem-posts-list/posts)
+
+(defstruct post title url author)
+
+(defun extract-posts (data)
+  (match data
+    ((plist :|data| (plist :|children| children))
+     (loop :for child :in children
+           :collect (match child
+                      ((plist :|data|
+                              (plist :|title| title
+                                     :|url| url
+                                     :|author| author))
+                       (make-post :title title
+                                  :url url
+                                  :author author)))))))
+
+(defun make-posts-url (subreddit)
+  (quri:make-uri :scheme "https"
+                 :host "reddit.com"
+                 :path (format nil "/r/~A/.json" subreddit)))
+
+(defun fetch-posts (subreddit)
+  (extract-posts (jojo:parse (dex:get (make-posts-url subreddit)))))
+```
+
+lem-posts-list/postsというパッケージに分離しました。
+fetch-postsという関数とpostのアクセサを外部から使いたいのでexportします。
+
+fetch-postsはsubreddit名を引数で受け取りpostのリストを返します。
+replから動作を確認してみます。
+
+```
+CL-USER> (ql:quickload :lem-posts-list/posts)
+CL-USER> (lem-posts-list/posts:fetch-posts "lisp")
+
+;; 出力は長いので省略
+```
+
+##
+投稿一覧が取得できるようになったので次はその内容をlemのバッファに表示し、選択して開けるようにします。
--- 02-Lem/posts-list/main.lisp
+++ 02-Lem/posts-list/main.lisp
@@ -5,23 +5,57 @@
   (:import-from #:trivia
                 #:match
                 #:plist)
-  (:use #:cl #:lem))
+  (:import-from #:quri)
+  (:import-from #:trivial-open-browser
+                #:open-browser)
+  (:use #:cl
+        #:lem
+        #:lem-posts-list/posts))
 (in-package #:lem-posts-list)
 
-(defstruct post title url author)
+(define-major-mode posts-list-mode nil
+    (:name "Posts list"
+     :keymap *posts-list-mode-keymap*)
+  (setf (buffer-read-only-p (current-buffer)) t))
 
-(defun extract-posts (data)
-  (match data
-    ((plist :|data| (plist :|children| children))
-     (loop :for child :in children
-           :collect (match child
-                      ((plist :|data|
-                              (plist :|title| title
-                                     :|url| url
-                                     :|author| author))
-                       (make-post :title title
-                                  :url url
-                                  :author author)))))))
+(define-key *posts-list-mode-keymap* "Return" 'posts-list-select)
+(define-key *posts-list-mode-keymap* "n" 'next-line)
+(define-key *posts-list-mode-keymap* "p" 'previous-line)
 
-(defun init ()
-  (jojo:parse (dex:get "http://reddit.com/r/lisp/.json")))
+(define-attribute author-attribute
+  (t :foreground "red"))
+
+(define-attribute title-attribute
+  (:light :foreground "blue")
+  (:dark :foreground "cyan"))
+
+(defun write-post (point post)
+  (with-point ((start point :right-inserting))
+    (insert-string point
+                   (format nil "[~A]" (post-author post))
+                   :attribute 'author-attribute)
+    (insert-string point " ")
+    (insert-string point
+                   (post-title post)
+                   :attribute 'title-attribute)
+    (insert-character point #\newline)
+    (put-text-property start point :post post)))
+
+(defun write-posts (buffer posts)
+  (with-buffer-read-only buffer nil
+    (erase-buffer buffer)
+    (let ((point (buffer-point buffer)))
+      (dolist (post posts)
+        (write-post point post))
+      (buffer-start point))))
+
+(define-command posts-list (subreddit) ("sSubreddit: ")
+  (let ((posts (fetch-posts subreddit))
+        (buffer (make-buffer (format nil "*Reddit ~A*" subreddit))))
+    (write-posts buffer posts)
+    (switch-to-buffer buffer)
+    (change-buffer-mode buffer 'posts-list-mode)))
+
+(define-command posts-list-select () ()
+  (let ((post (text-property-at (current-point) :post)))
+    (open-browser (post-url post))))
